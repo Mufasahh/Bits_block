a699a3bd1713a31cfcac43448e073457c2e92d9d
Mufasahh <Mufasahh1@gmail.com>
2024-10-11 02:09:10 +0100

    created and defined a structure to hold a hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use std::collections::HashMap;
+
+struct Country {
+    name: String,
+    population: HashMap<i32, i32>,
+    capital: String,
+}
+
 fn main() {
+    let my_city = Country {
+        name: "Nigeria".to_string(),
+        population: HashMap::new(),
+        capital: "Enugu".to_string(),
+    }
     println!("This is my first blockchain project using Rust");
     println!("I just got accepted to a top rated blockchain academy!");
 }
