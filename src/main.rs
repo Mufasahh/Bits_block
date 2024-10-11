92323bb31ac5bdb0153a971cbf5c01c2a3278598
Mufasahh <Mufasahh1@gmail.com>
2024-10-11 02:23:20 +0100

    I added an extra information for the country structure

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
+    my_country.population.insert(1900, 1_500_000);
+    my_country.population.insert(1960, 10_000_000);
+    my_country.population.insert(2022, 20_000_000);
+
+    for (year, population) in my_country.population {
+        println!("In {year} {0} had a population of {population}", my_country.name);
+    }
+    
     println!("This is my first blockchain project using Rust");
     println!("I just got accepted to a top rated blockchain academy!");
 }
