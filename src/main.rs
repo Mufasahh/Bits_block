e44d3a5384f3cc066f9cc965de63de91af9dc595
Mufasahh <Mufasahh1@gmail.com>
2024-10-11 02:28:46 +0100

    Edited a declaration error

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    my_country.population.insert(1900, 1_500_000);
-    my_country.population.insert(1960, 10_000_000);
-    my_country.population.insert(2022, 20_000_000);
+    my_city.population.insert(1900, 1_500_000);
+    my_city.population.insert(1960, 10_000_000);
+    my_city.population.insert(2022, 20_000_000);
 
-    for (year, population) in my_country.population {
-        println!("In {year} {0} had a population of {population}", my_country.name);
+    for (year, population) in my_city.population {
+        println!("In {year} {0} had a population of {population}", my_city.name);
     }
     
     println!("This is my first blockchain project using Rust");
