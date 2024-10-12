3c748fabe93304c95fac11be2972a2bb6622504f
Mufasahh <105505857+Mufasahh@users.noreply.github.com>
2024-10-12 15:32:05 +0100

    I created a basic pet structure

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,26 @@ struct Country {
     capital: String,
 }
 
+struct Pet {
+    name: String,
+    age: u32,
+}
+
 fn main() {
     let my_city = Country {
         name: "Nigeria".to_string(),
         population: HashMap::new(),
         capital: "Enugu".to_string(),
     }
+
+    let my_pet = Pet {
+        name: "Puffy".to_string(),
+        age = 10,
+    }
+    let Pet {
+        name,
+        age,
+    } = my_pet;
 
     my_city.population.insert(1900, 1_500_000);
     my_city.population.insert(1960, 10_000_000);
@@ -20,6 +34,8 @@ fn main() {
     for (year, population) in my_city.population {
         println!("In {year} {0} had a population of {population}", my_city.name);
     }
+
+    println!("My pet is called {name}, he is {age} years old);
     
 
     println!("This is my first blockchain project using Rust");
